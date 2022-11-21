3d2cb46c06662ab8986eba84fb8f28baa1745081
shubham khute <https://github.com/khuteshubh/Jake-ElwoodWebPageProjct.git>
2022-11-21 17:39:49 +0530

    new Project

---
+++ otpScreen.js
@@ -0,0 +1,22 @@
+let bodyTag = document.body;
+document.addEventListener("keydon",(e)=>{
+    console.log(0)
+})
+
+let otpInputBoxesTag = document.querySelectorAll(".otp-num")
+console.log(otpInputBoxesTag)
+
+otpInputBoxesTag.forEach((element,index)=>{
+    element.addEventListener("keydown",(e)=>{
+        if(e.key >= 0 && e.key<10){
+            otpInputBoxesTag[index].value = "";
+            setTimeout({} => {
+                otpInputBoxesTag[index+1].focus()
+            },0);
+        }
+        else if(e.key === 'Backspace'){
+            otpInputBoxesTag[index-1].focus();
+        }
+        console.log(index);
+    })
+});
